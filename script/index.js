175842ae06d47d7b41daf23951ca338794cb5402
Jesus Rodolfo <jesus.vacca99@gmail.com>
2024-08-03 18:07:34 -0500

    Creando la sección de mis habilidades

--- script/index.js
+++ script/index.js
@@ -29,3 +29,124 @@ document.getElementById('hamburguesa').addEventListener('click',function(){
     this.classList.toggle('open');
     document.getElementById('nav').classList.toggle('open');
 })
+
+const mySkllis = [
+    {
+        title:"Java",
+        porcentaje:90,
+        type:"lenguaje"
+    },
+    {
+        title:"Python",
+        porcentaje:70,
+        type:"lenguaje"
+    },
+    {
+        title:"JavaScript",
+        porcentaje:40,
+        type:"lenguaje"
+    },
+    {
+        title:"Spring boot",
+        porcentaje:60,
+        type:"frameWork"
+    },
+    {
+        title:"Django",
+        porcentaje:40,
+        type:"frameWork"
+    },
+    {
+        title:"MySql",
+        porcentaje:50,
+        type:"db"
+    },
+    {
+        title:"Html",
+        porcentaje:80,
+        type:"maquetado"
+    },
+    {
+        title:"Css",
+        porcentaje:70,
+        type:"estilos"
+    },
+    
+];
+
+function loadSkllis(type = "all"){
+    const content = document.getElementById('contentSkllis');
+    content.innerHTML = '';
+    
+
+    if(type == "all"){
+        mySkllis.forEach(sklli=>{
+            const item = document.createElement('div');
+            const progress = document.createElement('div');
+            const contentProgress = document.createElement('div');
+            const h3 = document.createElement('h3');
+    
+            // Asignamos las clases
+            item.className = 'items__skills';
+            progress.className = 'item__progress'
+            contentProgress.className = 'content__progress';
+    
+            h3.innerHTML = sklli.title;
+            const porcentaje = sklli.porcentaje * 100 / 100;
+            progress.style.width = `${porcentaje}%`;
+    
+            contentProgress.appendChild(progress);
+            item.append(h3,contentProgress);
+    
+            content.appendChild(item);
+        })
+    }else{
+        const data = mySkllis.filter(skill => skill.type == type);
+        data.forEach(sklli=>{
+            const item = document.createElement('div');
+            const progress = document.createElement('div');
+            const contentProgress = document.createElement('div');
+            const h3 = document.createElement('h3');
+    
+            // Asignamos las clases
+            item.className = 'items__skills';
+            progress.className = 'item__progress'
+            contentProgress.className = 'content__progress';
+    
+            h3.innerHTML = sklli.title;
+            const porcentaje = sklli.porcentaje * 100 / 100;
+            progress.style.width = `${porcentaje}%`;
+    
+            contentProgress.appendChild(progress);
+            item.append(h3,contentProgress);
+    
+            content.appendChild(item);
+        });
+    }
+
+   
+}
+const listButton = document.querySelectorAll('.nav__skills button');
+listButton.forEach((button, index)=>{
+    button.addEventListener('click',function(){
+        listButton.forEach(b=>{
+            if(b.classList.contains('focus')) b.classList.remove('focus');
+        });
+        button.classList.add('focus');
+        switch (index) {
+            case 0:
+                loadSkllis();
+                break;
+            case 1:
+                loadSkllis("lenguaje");
+                break;
+            case 2:
+                loadSkllis("frameWork");
+                break;
+            case 3:
+                loadSkllis("db");
+                break;
+        }
+    })
+})
+listButton[0].click();
